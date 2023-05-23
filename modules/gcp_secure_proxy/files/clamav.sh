4d7e64be1d361928a5a1f0543693383706a3b049
h.belgacem <hassene@belgacem.io>
2023-05-23 22:29:21 +0200

    add ClamAv.cc.patch

--- modules/gcp_secure_proxy/files/clamav.sh
+++ modules/gcp_secure_proxy/files/clamav.sh
@@ -18,6 +18,26 @@ wget https://www.e-cap.org/archive/ecap_clamav_adapter-2.0.0.tar.gz
 # unpack
 tar -xvzf ecap_clamav_adapter-2.0.0.tar.gz
 
+cat << 'EOF' > ClamAv.cc.patch
+--- ClamAv.cc	2015-11-08 13:07:35.000000000 -0500
++++ ClamAv.cc.new	2019-07-29 08:34:21.000000000 -0400
+@@ -44,8 +44,13 @@
+     // We assume that cl_*() functions used here are threadsafe.
+
+     const char *virname = 0;
+-    const int eScanResult = cl_scanfile(answer.fileName.c_str(), &virname, 0, engine, CL_SCAN_STDOPT);
+
++    static struct cl_scan_options options = {};
++    {
++        options.parse |= ~0; // enable all parsers
++    }
++    const int eScanResult = cl_scanfile(answer.fileName.c_str(), &virname, 0, engine, &options);
++
+     switch (eScanResult) {
+     case CL_CLEAN:
+         answer.statusCode = Answer::scClean;
+EOF
+
 # patch the CL_SCAN_STDOPT error
 patch ecap_clamav_adapter-2.0.0/src/ClamAv.cc < ClamAv.cc.patch
 
